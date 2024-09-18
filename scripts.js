f6e4d7fb7e56383091bef064c8cb30714d89cc8e
jorgesm25 <155186295+jorgesm25@users.noreply.github.com>
2024-09-18 12:46:21 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,48 +2,7 @@ function changeChart(chart) {
     document.getElementById('chart-iframe').src = chart;
 }
 
-function mostrarMensaje(mensaje, esError) {
-    if (esError) {
-        console.error(mensaje);
-        alert("Error:\n" + mensaje);
-    } else {
-        console.log(mensaje);
-        alert("Mensaje:\n" + mensaje);
-    }
-}
 
-function enviarDatos() {
-    // Simulando datos a enviar
-    var datosSimulados = {
-        nombre: "Ejemplo Nombre",
-        genero: "Masculino",
-        area: "Estadística",
-        pais: "México",
-        perfil: "Estudiante"
-    };
-
-    // Simulación de envío de datos
-    fetch('recibirForm.php', {
-        method: 'POST',
-        body: JSON.stringify(datosSimulados),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            mostrarMensaje('Datos enviados con éxito', false);
-        } else {
-            mostrarMensaje(data.message, true);
-        }
-    })
-    .catch(error => {
-        mostrarMensaje('Error al enviar los datos', true);
-    });
-
-    return false;
-}
 
 let index = 0;
 
